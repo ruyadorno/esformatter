0b0ad7117b844ca1917384e72a88733d5546f794
Ruy Adorno <ruyadorno@hotmail.com>
2014-10-13 16:48:38 -0400

    Starting base structure

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
+(function (React, ace) {
+
+  'use strict';
+
+  var editor = ace.edit("editor");
+  editor.setTheme("ace/theme/twilight");
+  editor.getSession().setMode("ace/mode/javascript");
+
+  var editorResult = ace.edit("editor-result");
+  editorResult.setTheme("ace/theme/twilight");
+  editorResult.getSession().setMode("ace/mode/javascript");
+
+})(window.React, window.ace);
 
